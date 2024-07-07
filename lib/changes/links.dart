2abcd35613f1e822a3c4335bc18a12fd9327ace0
Dheeraj Prajapat <dheerajprajapati334@gmail.com>
2024-07-07 18:49:28 +0530

    Initial commit

---
+++ lib/changes/links.dart
@@ -0,0 +1,17 @@
+// CV
+const String resume =
+    'https://drive.google.com/file/d/118mPDS3KYaHHPnGptWDbU0d3qvI18MRc/view?usp=sharing';
+
+// Github Link
+const String gitHub = 'https://github.com/Iamdheeraj22';
+
+// Linkdin
+const String linkedin = 'https://linkedin.com/in/iamdheeraj24';
+
+// Github Link
+const String instagram = 'https://instagram.com/iamdheeraj21';
+
+const String profileImage =
+    "https://images.unsplash.com/photo-1503443207922-dff7d543fd0e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=627&q=80";
+// WhatsApp
+const String whatsapp = 'https://api.whatsapp.com/send?phone=8290845089';
---
+++ lib/changes/strings.dart
@@ -0,0 +1,26 @@
+String hellotag = '''Hi there, Welcome to My Space  ''';
+
+String yourname = "I'm Dheeraj Prajapat,";
+// String hometitle2 = "A Mobile Application\nDeveloper";
+
+String animationtxt1 = " Mobile Application Developer";
+
+//
+String contactHeadding = "Let’s try my service now!";
+String contactSubHeadding =
+    "Let’s work together and make everything super cute and super useful.";
+//
+String miniDescription =
+    "Freelancer providing services for programming and design content needs. Join me down below and let's get started!";
+
+String servicesSubHeading =
+    "Based on my experience, I specialize in creating fluid and responsive mobile applications using Flutter and Dart. I have developed and maintained several high-quality apps, integrating APIs, implementing intuitive UI designs, and managing backend services. My expertise also includes using FlutterFlow for rapid development and Firebase for backend solutions.";
+//
+
+String protfolioSubHeading =
+    "Since the beginning of my journey as a designer and developer, I have created digital products for business and consumer use. This is a little bit.";
+const String aboutMeHeadline =
+    'I\'m Sudesh Bandara, a Mobile App developer, Web developer and UI designer.';
+
+const String aboutMeDetail =
+    'I\'m a Fresh Graduate as Software Engineer from Quanrt University, UK. I have been developing mobile apps for over 4 years now. I have worked in teams for various startups and helped them in launching their prototypes and got valuable learning experience. Been a CEO/Co-Founder of Onyxsio, Sri Lanka. Currently working as an Associate Software Engineer at Onyxsio.';
