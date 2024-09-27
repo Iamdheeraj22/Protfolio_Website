2e9aea9e19f913528be1020ce9b01f386f592cb6
Dheeraj Prajapat <dheerajprajapati334@gmail.com>
2024-09-27 20:28:48 +0530

    feat : implement separate functionality of main page

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,52 @@
 import 'package:flutter/material.dart';
-import 'my_site.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:mysite/core/configs/connection/bloc/connected_bloc.dart';
+import 'package:mysite/core/configs/connection/network_check.dart';
+import 'package:mysite/core/providers/drawer_provider.dart';
+import 'package:mysite/core/providers/scroll_provider.dart';
+import 'package:mysite/core/theme/app_theme.dart';
+import 'package:mysite/core/theme/cubit/theme_cubit.dart';
+import 'package:provider/provider.dart';
+import 'package:sizer/sizer.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(const MySite());
+  runApp(const PortfolioWeb());
+}
+
+class PortfolioWeb extends StatelessWidget {
+  const PortfolioWeb({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<ThemeCubit>(create: (_) => ThemeCubit()),
+        BlocProvider<ConnectedBloc>(create: (context) => ConnectedBloc()),
+      ],
+      child: MultiProvider(
+        providers: [
+          ChangeNotifierProvider(create: (_) => DrawerProvider()),
+          ChangeNotifierProvider(create: (_) => ScrollProvider()),
+        ],
+        child: BlocBuilder<ThemeCubit, ThemeState>(
+          builder: (context, state) {
+            return Sizer(
+              builder: (context, orientation, deviceType) {
+                return MaterialApp(
+                  debugShowCheckedModeBanner: false,
+                  title: 'Dheeraj Prajapat | Mobile Application Developer',
+                  theme: AppTheme.themeData(state.isDarkThemeOn, context),
+                  initialRoute: "/",
+                  routes: {
+                    "/": (context) => const NChecking(),
+                  },
+                );
+              },
+            );
+          },
+        ),
+      ),
+    );
+  }
 }
