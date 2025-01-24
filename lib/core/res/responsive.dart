531d6f4ee40e1550f7d972d160c5104d6ebf059c
Dheeraj Prajapat <dheerajprajapati334@gmail.com>
2025-01-24 17:16:23 +0530

    feat: handle two more different screens sizes

--- lib/core/res/responsive.dart
+++ lib/core/res/responsive.dart
@@ -4,31 +4,46 @@ class Responsive extends StatelessWidget {
   final Widget mobile;
   final Widget tablet;
   final Widget desktop;
+
+  final Widget? extraLargeScreen;
+  final Widget? largeMobile;
+
   const Responsive({
     Key? key,
     required this.mobile,
     required this.tablet,
     required this.desktop,
+    this.extraLargeScreen,
+    this.largeMobile,
   }) : super(key: key);
 
   // screen sizes
   static bool isMobile(BuildContext context) =>
       MediaQuery.of(context).size.width <= 500;
 
+  static bool isLargeMobile(BuildContext context) =>
+      MediaQuery.of(context).size.width <= 700;
+
   static bool isTablet(BuildContext context) =>
-      MediaQuery.of(context).size.width < 1024 &&
-      MediaQuery.of(context).size.width >= 600;
+      MediaQuery.of(context).size.width < 1024;
 
   static bool isDesktop(BuildContext context) =>
       MediaQuery.of(context).size.width >= 1024;
 
+  static bool isExtraLargeScreen(BuildContext context) =>
+      MediaQuery.sizeOf(context).width > 1400;
+
   @override
   Widget build(BuildContext context) {
-    var width = MediaQuery.of(context).size.width;
-    if (width >= 1024) {
+    final Size size = MediaQuery.of(context).size;
+    if (size.width > 1400 && extraLargeScreen != null) {
+      return extraLargeScreen!;
+    } else if (size.width >= 1080) {
       return desktop;
-    } else if (width < 1024 && width >= 600) {
+    } else if (size.width >= 700) {
       return tablet;
+    } else if (size.width >= 500 && largeMobile != null) {
+      return largeMobile!;
     } else {
       return mobile;
     }
