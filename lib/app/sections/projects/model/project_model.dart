6ff401b2e323691f7a7ffd016e2f9180de745c14
Dheeraj Prajapat <dheerajprajapati334@gmail.com>
2025-01-24 17:15:35 +0530

    refactor: Customise project model

--- lib/app/sections/projects/model/project_model.dart
+++ lib/app/sections/projects/model/project_model.dart
@@ -1,53 +1,43 @@
 class ProjectModel {
-  final String pName;
-  final bool isPersonal;
-  final List<String> techStacks;
-  final String? playStore;
-  final String? appleStore;
-  final String? github;
+  final String projectName;
+  final String projectDescription;
+  final String projectLink;
 
   ProjectModel({
-    required this.pName,
-    required this.isPersonal,
-    required this.techStacks,
-    this.github,
-    this.playStore,
-    this.appleStore,
+    required this.projectName,
+    required this.projectDescription,
+    required this.projectLink,
   });
 }
 
 List<ProjectModel> projects = [
   ProjectModel(
-    pName: 'Montra',
-    isPersonal: false,
-    techStacks: [
-      'Flutter',
-      'Dart',
-      'Firebase',
-      'Aws Code Commit',
-      'Api Integration'
-    ],
+    projectName: "Portfolio Website",
+    projectDescription:
+        "The Portfolio Website, built with the Flutter framework, "
+        "is a visually appealing and fully responsive platform designed to showcase personal or professional projects."
+        " It dynamically adjusts its layout to provide an optimal user experience across devices, including mobile, tablet,"
+        " and desktop. With smooth animations, intuitive navigation, and a modern design, the website effectively highlights skills, "
+        "achievements, and work in a professional manner.",
+    projectLink: "https://github.com/Iamdheeraj22/Protfolio_Website",
   ),
   ProjectModel(
-    pName: 'Montra',
-    isPersonal: false,
-    techStacks: [
-      'Flutter',
-      'Dart',
-      'Firebase',
-      'Aws Code Commit',
-      'Api Integration'
-    ],
+    projectName: "Blog App",
+    projectDescription:
+        "The Blog App is a feature-rich application built using Flutter and powered by Supabase for backend services. "
+        "It offers seamless user authentication and allows users to create, read, and manage blogs efficiently."
+        " Blogs are securely stored in Supabase's storage, ensuring reliability and fast access."
+        " With an intuitive interface and smooth performance, the app provides a great platform for users to share "
+        "and explore content effortlessly.",
+    projectLink: "https://github.com/Iamdheeraj22/blog_app",
   ),
   ProjectModel(
-    pName: 'Montra',
-    isPersonal: false,
-    techStacks: [
-      'Flutter',
-      'Dart',
-      'Firebase',
-      'Aws Code Commit',
-      'Api Integration'
-    ],
+    projectName: "WallPaper Hub",
+    projectDescription: "Wallpaper Hub is a modern app built with Flutter, "
+        "offering a vast collection of high-quality wallpapers across various categories like nature,"
+        " abstract, and minimalistic designs. With a smooth and user-friendly interface, users can easily browse,"
+        " search, and download wallpapers to personalise their devices. The app ensures fast performance and"
+        " regularly updates its content to keep the collection fresh and engaging.",
+    projectLink: "https://github.com/Iamdheeraj22/wallpaperhub",
   )
 ];
